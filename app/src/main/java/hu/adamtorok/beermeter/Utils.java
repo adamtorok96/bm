f3767132afacd1020c54b50d592cf6d329b1f87a
edems <adamtorok@sch.bme.hu>
2018-11-06 16:39:48 +0100

    Utils added

---
+++ app/src/main/java/hu/adamtorok/beermeter/Utils.java
@@ -0,0 +1,28 @@
+package hu.adamtorok.beermeter;
+
+import android.os.Looper;
+
+import java.util.concurrent.TimeUnit;
+
+public class Utils {
+
+    public static double vectorLength(float[] vector) {
+        return Math.sqrt(
+                vector[0] * vector[0] +
+                vector[1] * vector[1] +
+                vector[2] * vector[2]
+        );
+    }
+
+    public static boolean isMainThread() {
+        return Looper.myLooper() == Looper.getMainLooper();
+    }
+
+    public static long nanoTimeToSeconds(long time) {
+        return TimeUnit.SECONDS.convert(time, TimeUnit.NANOSECONDS);
+    }
+
+    public static long nanoTimeToMilliSeconds(long time) {
+        return TimeUnit.MICROSECONDS.convert(time, TimeUnit.NANOSECONDS);
+    }
+}
